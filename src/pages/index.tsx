9678200af18ea454c7e72597f6540369174cf082
Ignacio Latorre <ignaciolatorreap@gmail.com>
2023-02-24 15:48:08 -0300

    relative paths second fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,52 +8,52 @@ import wines from '@/static/wines.json';
 const brandonGrotesque = localFont({ 
   src: [
     {
-      path: "@/assets/fonts/Brandon_light.otf",
+      path: "src/assets/fonts/Brandon_light.otf",
       weight: "300",
       style: "normal"
     },
     {
-      path: "@/assets/fonts/Brandon_light_it.otf",
+      path: "src/assets/fonts/Brandon_light_it.otf",
       weight: "300",
       style: "italic"
     },
     {
-      path: "@/assets/fonts/Brandon_reg.otf",
+      path: "src/assets/fonts/Brandon_reg.otf",
       weight: "400",
       style: "normal"
     },
     {
-      path: "@/assets/fonts/Brandon_reg_it.otf",
+      path: "src/assets/fonts/Brandon_reg_it.otf",
       weight: "400",
       style: "italic"
     },
     {
-      path: "@/assets/fonts/Brandon_med.otf",
+      path: "src/assets/fonts/Brandon_med.otf",
       weight: "500",
       style: "normal"
     },
     {
-      path: "@/assets/fonts/Brandon_med_it.otf",
+      path: "src/assets/fonts/Brandon_med_it.otf",
       weight: "500",
       style: "italic"
     },
     {
-      path: "@/assets/fonts/Brandon_bld.otf",
+      path: "src/assets/fonts/Brandon_bld.otf",
       weight: "600",
       style: "normal"
     },
     {
-      path: "@/assets/fonts/Brandon_bld_it.otf",
+      path: "src/assets/fonts/Brandon_bld_it.otf",
       weight: "600",
       style: "italic"
     },
     {
-      path: "@/assets/fonts/Brandon_blk.otf",
+      path: "src/assets/fonts/Brandon_blk.otf",
       weight: "700",
       style: "normal"
     },
     {
-      path: "@/assets/fonts/Brandon_blk_it.otf",
+      path: "src/assets/fonts/Brandon_blk_it.otf",
       weight: "700",
       style: "italic"
     }
