2c373ed5b467adcb7e720f99d430be57f71b93bd
Ignacio Latorre <ignaciolatorreap@gmail.com>
2023-03-05 21:33:38 -0300

    image path fix 4th try

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: ghPages ? '/src/' : '',
-  assetPrefix: ghPages ? '/public/' : '',
+  basePath: ghPages ? '/gaia-box-gourmet-web/' : '',
 }
 
 module.exports = nextConfig
