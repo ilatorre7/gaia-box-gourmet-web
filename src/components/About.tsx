3a6150c685539e382f92a635a3229eb1e3d97cae
Ignacio Latorre <ignaciolatorreap@gmail.com>
2023-03-06 16:29:34 -0300

    image fix 5th try

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,7 +30,7 @@ function About() {
         </SectionText>
       </div>
       <div className='image w-full lg:w-1/2 px-4 flex justify-center items-center'>
-        <Image src={`${prefix}/about.png`} alt="Productos de Gaia Box" width={884} height={590} className='rounded-xl shadow-xl' />
+        <Image src={`gaia-box-gourmet-web/public/about.png`} alt="Productos de Gaia Box" width={884} height={590} className='rounded-xl shadow-xl' />
       </div>
     </div>
   )
